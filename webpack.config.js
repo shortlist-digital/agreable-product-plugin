9098aeda86c9992ed3be6166d4394899bf378ad4
thomasdigby <thomasdigby1@gmail.com>
2016-09-01 11:38:12 +0100

    In progress production set-up and files

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,22 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin")
 var buildPath = path.resolve(__dirname, 'resources', 'assets');
 var mainPath = path.resolve(__dirname, 'src', 'main.js');
 
+console.log(buildPath)
+
 module.exports = {
   entry: mainPath,
   output: {
     path: buildPath,
-    filename: 'app.js'
+    filename: 'app.js',
+    publicPath: ''
   },
   module: {
     loaders: [
-      { test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader?paths[]=./src/styles&paths[]=../../themes/agreable-app-theme/styles&paths[]=./node_modules'},
+      { test: /\.styl$/, loader: ExtractTextPlugin.extract('style', 'css!stylus?paths[]=./src/styles&paths[]=../../themes/agreable-app-theme/styles&paths[]=./node_modules')},
       { test: /\.svg$/, exclude:'/node_modules/', loader: 'raw-loader' },
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.woff$|.eot$|.svg$|.ttf$|.png$|.gif$|.jpg$|.jpeg$/, loader: "url" },
-      { test: /src\/.*\.jsx?$/, loaders: ['react-hot', 'babel-loader?stage=0&loose=all'], exclude: '/node_modules/',  include: path.join(__dirname, "src") }
+      { test: /\.js$/, loader: 'babel?presets[]=es2015', include: path.join(__dirname, "src") }
     ]
   },
 
