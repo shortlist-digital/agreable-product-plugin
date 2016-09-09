e3bd1e5d2ece62d44d56ee063349f887fb315ca0
thomasdigby <thomasdigby1@gmail.com>
2016-09-09 19:10:48 +0100

    Fixed async JS build for dev/prod

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import DOMReady from 'detect-dom-ready'
 import LazyImages from './javascript/lazy-images'
 import ProductCTA from './javascript/product-cta'
 import Share from './javascript/share/share.js'
@@ -6,13 +5,11 @@ import SiteHeaderSticky from './javascript/site-header'
 import SiteNavigation from './javascript/site-navigation'
 import './stylus/main.styl'
 
-DOMReady(function () {
-  // IS THIS INFO NECESSARY?
-  // Information about the promotion is bootstrapped from the server
-  // var agreablePromoData = window.agreablePromoData
-  new SiteNavigation()
-  new LazyImages()
-  new Share()
-  new ProductCTA()
-  new SiteHeaderSticky()
-})
+// IS THIS INFO NECESSARY?
+// Information about the promotion is bootstrapped from the server
+// var agreablePromoData = window.agreablePromoData
+new SiteNavigation()
+new LazyImages()
+new Share()
+new ProductCTA()
+new SiteHeaderSticky()
